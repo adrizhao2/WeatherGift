43fac5e588d365b63eb27a2ef59005cb8f1e704f
Adrianna Zhao <zhaocx@bc.edu>
2019-06-17 17:36:30 -0400

    7.10 Complete

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class PageVC: UIPageViewController {
-
+    
     var currentPage = 0
-    var locationsArray = ["Local City", "Sydney, Australia", "Shanghai, China", "Tokyo, Japan"]
+    var locationsArray = [WeatherLocation]()
     var pageControl: UIPageControl!
     var barButtonWidth: CGFloat = 44
     var barButtonHeight: CGFloat = 44
@@ -22,6 +22,10 @@ class PageVC: UIPageViewController {
         
         delegate = self
         dataSource = self
+        
+        var newLocation = WeatherLocation()
+        newLocation.name = "Unknown Weather Location"
+        locationsArray.append(newLocation)
         
         setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
     }
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Adrianna Zhao on 6/17/19.
+//  Copyright © 2019 Adrianna Zhao. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
