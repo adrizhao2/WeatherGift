579411025ec2d154a067417e1bb055b9d208be68
Adrianna Zhao <zhaocx@bc.edu>
2019-06-18 08:14:28 -0400

    7.15 Complete

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Adrianna Zhao on 6/18/19.
+//  Copyright © 2019 Adrianna Zhao. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "a01d52eda4dccec79b88c6561fe3efed/"
